7e290df133cffa0f61c7961937098c013c6072a4
Jacky Lin <mr7.jacky@gmail.com>
2022-04-07 20:57:43 -0400

    new v

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -96,7 +96,7 @@ function Sidebar(props) {
   };
   const { routes, logo } = props;
   let navbarBrand = (
-    <NavbarBrand href="/admin/dashboard" className="pt-0">
+    <NavbarBrand href="/admin/dashboard" onClick={router.reload()} className="pt-0">
       <img alt={logo.imgAlt} className="navbar-brand-img" src={logo.imgSrc} />
     </NavbarBrand>
   );
